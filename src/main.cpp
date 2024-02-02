ee0de22fe3b66ff43246d63a3f0344f4a56d3780
Ryuaraki <ryutaro.araki@woven.toyota>
2024-02-02 10:41:34 +0900

    srcの微修正. citymap.csvの更新. README.mdの記述

--- src/main.cpp
+++ src/main.cpp
@@ -104,6 +104,7 @@ int main () {
             std::cout << "~~~~~~~~~~~~~~~~~~~~" << std::endl;
             std::cout << "~~~~ Goalに到着 ~~~~~" << std::endl;
             std::cout << "~~~~~~~~~~~~~~~~~~~~" << std::endl;
+            break;
         } else if (MyCar.fuel <= 0) {
             std::cout << "###############################################" << std::endl;
             std::cout << "#############  Fuel is empty  #################" << std::endl;
